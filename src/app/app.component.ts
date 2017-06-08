7cbe1b3df93d47efde19a303a0789bca46f5a8fb
goodnesskay <gtkbrain@gmail.com>
2017-06-08 09:10:07 +0100

    restructured folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: '../assets/templates/app.component.html',
+  styleUrls: ['../assets/css/app.component.css']
 })
 export class AppComponent {
   title = 'Auth-Angular';
